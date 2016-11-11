a815d865b772182485da82964b5950560c3ec099
diatmpravin <pravinmishra88@gmail.com>
2016-11-11 14:17:06 +0530

    rails with dockerfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WithoutDatabase::Application.config.secret_token = '5efb013f22861a9957d373d4b34b6e2aca6f3d841e5125cb0f0059cbc8424deeffe36fc1fc689f669ce57c8bc32e9802cdb216f4f4fdff7064fd2b72e13743ca'
